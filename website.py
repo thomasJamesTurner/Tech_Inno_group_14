c4f71206759193b4b8016dd8822460fb7da88078
13Ruh <s5625484@bournemouth.ac.uk>
2025-04-28 18:07:05 +0100

    copy

--- website.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-from django.shortcuts import render
-
-def Home(request):
-    current_time = datetime.now().strftime('%H:%M')
-    return render(request, 'Home.html', {'current_time': current_time})

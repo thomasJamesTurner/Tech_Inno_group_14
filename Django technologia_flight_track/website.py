e51ad8b7dc95c226bfb049827512699692fc052a
13Ruh <s5625484@bournemouth.ac.uk>
2025-04-29 01:01:03 +0100

    naming problems

--- Django technologia_flight_track/website.py
+++ Django technologia_flight_track/website.py
@@ -1,6 +1,12 @@
 from datetime import datetime
-from django.shortcuts import render
+from flask import Flask, render_template
 
-def Home(request):
+app = Flask(__name__, static_folder='Styles', template_folder='Templates')
+
+@app.route('/')
+def Home():
     current_time = datetime.now().strftime('%H:%M')
-    return render(request, 'Home.html', {'current_time': current_time})
+    return render_template('Home.html', current_time=current_time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
